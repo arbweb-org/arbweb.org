158eb7cf625f6353a210128a873b65c2755f1226
yassin-soliman <akcorp@hotmail.com>
2019-08-31 10:05:19 +0200

    ok

---
+++ arbweb-org/wpf.arbweb.org/_c_app.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace wpf.arbweb.org
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class _c_app : Application
+    {
+    }
+}
